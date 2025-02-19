57ecfa7bd8abb0084d65ccc49e5bd9ae767df035
Daniel <dacontre@andrew.cmu.edu>
2025-02-19 02:05:09 -0500

    poll modal working pretty well

---
+++ public/src/client/topic/poll.js
@@ -0,0 +1,92 @@
+'use strict';
+
+define('forum/topic/poll', [
+	'components', 'translator', 'alerts',
+], function (components, translator, alerts) {
+	const Poll = {};
+
+	let pollModal;
+	let pollOptions = [];
+
+	Poll.init = function () {
+		if (pollModal) {
+			return;
+		}
+
+		app.parseAndTranslate('modals/create-poll', {}, function (html) {
+			pollModal = html;
+			$('body').append(pollModal);
+
+			// Get Elements
+			const inputField = pollModal.find('#topicId');
+			const addButton = pollModal.find('#add_option');
+			const createPollButton = pollModal.find('#create_poll');
+			const optionsContainer = pollModal.find('#poll-options-list');
+
+			// Initially disable buttons
+			addButton.prop('disabled', true);
+			createPollButton.prop('disabled', true);
+
+			// Event Listeners
+			pollModal.find('#move_posts_cancel').on('click', closePollModal);
+
+			// Enable "Add Option" button when input is not empty
+			inputField.on('input', function () {
+				const inputText = inputField.val().trim();
+				addButton.prop('disabled', inputText === '');
+			});
+
+			// Add option when button is clicked
+			addButton.on('click', function () {
+				addPollOption(inputField, optionsContainer, addButton, createPollButton);
+			});
+		});
+	};
+
+	function addPollOption(inputField, optionsContainer, addButton, createPollButton) {
+		const optionText = inputField.val().trim();
+		if (optionText === '') {
+			return;
+		}
+
+		// Add to the options list
+		pollOptions.push(optionText);
+
+		// Create option element and append it to the container
+		const optionElement = $(`
+			<div class="poll-option-item">
+				<span>${optionText}</span>
+				<button class="btn btn-sm btn-danger remove-option">✖</button>
+			</div>
+		`);
+		optionsContainer.append(optionElement);
+
+		// Clear input field and disable "Add Option" button
+		inputField.val('');
+		addButton.prop('disabled', true);
+
+		// Enable "Create Poll" button when at least one option is added
+		createPollButton.prop('disabled', pollOptions.length === 0);
+
+		// Handle removing an option
+		optionElement.find('.remove-option').on('click', function () {
+			const index = pollOptions.indexOf(optionText);
+			if (index > -1) {
+				pollOptions.splice(index, 1);
+			}
+			optionElement.remove();
+
+			// Disable "Create Poll" button if no options remain
+			createPollButton.prop('disabled', pollOptions.length === 0);
+		});
+	}
+
+	function closePollModal() {
+		if (pollModal) {
+			pollModal.remove();
+			pollModal = null;
+		}
+	}
+
+	return Poll;
+});
--- src/views/modals/create-poll.tpl
+++ src/views/modals/create-poll.tpl
@@ -1,24 +1,23 @@
-<div class="card tool-modal shadow">
+<div class="card tool-modal shadow" style="width: 500px; height: 450px;">
 	<h5 class="card-header">
 		Create Poll
 	</h5>
 	<div class="card-body">
-		<p>
-            Write your options below
-        </p>
+		<p>Write your options below</p>
 		<div>
-			<label class="form-label" for="topicId"><strong>Optons</strong></label>
+			<label class="form-label" for="topicId"><strong>Options</strong></label>
 			<input id="topicId" type="text" class="form-control"><br/>
 		</div>
-        <button class="btn btn-primary btn-sm" id="move_posts_confirm" disabled>Add Option</button>
-		<p>
-    		<strong><span id="pids"></span></strong>
-        </p>
+        <button class="btn btn-primary btn-sm" id="add_option" disabled>Add Option</button>
+
+		<!-- Scrollable container for poll options -->
+		<div id="poll-options-container" style="max-height: 150px; overflow-y: auto; border: 1px solid #ccc; padding: 10px; margin-top: 10px;">
+			<!-- Poll options will be inserted here dynamically -->
+			<div id="poll-options-list"></div>
+		</div>
 	</div>
 	<div class="card-footer text-end">
-		<button class="btn btn-link btn-sm" id="move_posts_cancel">[[global:buttons.close]]</button>
-		<button class="btn btn-primary btn-sm" id="move_posts_confirm" disabled>[[topic:move]]</button>
+		<button class="btn btn-link btn-sm" id="move_posts_cancel">Close</button>
+		<button class="btn btn-primary btn-sm" id="create_poll" disabled>Create Poll</button>
 	</div>
 </div>
-
-
