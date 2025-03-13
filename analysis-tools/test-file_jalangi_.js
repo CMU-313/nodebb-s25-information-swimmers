4c5a804ea59e29cd6c83db4539029ac2a4b8d2ba
Bo Xie <boxie@andrew.cmu.edu>
2025-03-13 01:19:15 -0400

    Add Jalangi2 dynamic analysis tool

---
+++ analysis-tools/test-file_jalangi_.js
@@ -0,0 +1,19 @@
+J$.iids = {"9":[1,1,1,8],"17":[1,13,1,29],"25":[1,1,1,30],"27":[1,1,1,12],"33":[1,1,1,31],"41":[1,1,1,31],"49":[1,1,1,31],"57":[1,1,1,31],"nBranches":0,"originalCodeFileName":"/Users/xiebo/Desktop/17313/nodebb-s25-information-swimmers-2/analysis-tools/test-file.js","instrumentedCodeFileName":"/Users/xiebo/Desktop/17313/nodebb-s25-information-swimmers-2/analysis-tools/test-file_jalangi_.js","code":"console.log(\"Hello Jalangi2\");"};
+jalangiLabel0:
+    while (true) {
+        try {
+            J$.Se(41, '/Users/xiebo/Desktop/17313/nodebb-s25-information-swimmers-2/analysis-tools/test-file_jalangi_.js', '/Users/xiebo/Desktop/17313/nodebb-s25-information-swimmers-2/analysis-tools/test-file.js');
+            J$.X1(33, J$.M(25, J$.R(9, 'console', console, 2), 'log', 0)(J$.T(17, "Hello Jalangi2", 21, false)));
+        } catch (J$e) {
+            J$.Ex(49, J$e);
+        } finally {
+            if (J$.Sr(57)) {
+                J$.L();
+                continue jalangiLabel0;
+            } else {
+                J$.L();
+                break jalangiLabel0;
+            }
+        }
+    }
+// JALANGI DO NOT INSTRUMENT
