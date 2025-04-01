f5941896da994a61c9afd3f74a48ffa1d09e559f
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2025-04-01 14:48:49 -0400

    linter fixes

--- test/api.js
+++ test/api.js
@@ -615,8 +615,6 @@ describe('API', async () => {
 		// Compare the schema to the response
 		required.forEach((prop) => {
 			if (schema.hasOwnProperty(prop)) {
-				// assert(response.hasOwnProperty(prop), `"${prop}" is a required property (path: ${method} ${path}, context: ${context})`);
-
 				// Don't proceed with type-check if the value could possibly be unset (nullable: true, in spec)
 				if (response[prop] === null && schema[prop].nullable === true) {
 					return;
@@ -627,7 +625,6 @@ describe('API', async () => {
 
 				switch (schema[prop].type) {
 					case 'string':
-						// assert.strictEqual(typeof response[prop], 'string', `"${prop}" was expected to be a string, but was ${typeof response[prop]} instead (path: ${method} ${path}, context: ${context})`);
 						break;
 					case 'boolean':
 						assert.strictEqual(typeof response[prop], 'boolean', `"${prop}" was expected to be a boolean, but was ${typeof response[prop]} instead (path: ${method} ${path}, context: ${context})`);
@@ -662,10 +659,8 @@ describe('API', async () => {
 		// Compare the response to the schema
 		Object.keys(response).forEach((prop) => {
 			if (additionalProperties) { // All bets are off
-				return;
+				// return;
 			}
-
-			// assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
 		});
 	}
 });
