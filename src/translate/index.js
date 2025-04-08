4defa5c9e52c4795e1cf569802adf945b1266025
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2025-04-08 10:31:17 -0400

    edit translator url

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,7 +6,8 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
-	const TRANSLATOR_API = 'http://17313-team02.s3d.cmu.edu:5000';
+	// const TRANSLATOR_API = 'http://17313-team02.s3d.cmu.edu:5000';
+	const TRANSLATOR_API = 'http://translator:5000';
 	try {
 		const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 		const data = await response.json();
